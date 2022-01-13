366a441fd70737f0a1d29761075abb77bcc0a896
synchronEnginee <te2.9niku.41cd@gmail.com>
2022-01-13 23:10:53 +0900

    welcome page

--- src/navigation/RootStackNav.tsx
+++ src/navigation/RootStackNav.tsx
@@ -1,12 +1,12 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import {Home, Instructions} from 'screens';
+import {Welcome, Instructions} from 'screens';
 
 const nav = createStackNavigator();
 export const RootStackNav: React.FC = () => {
   return (
-    <nav.Navigator initialRouteName={Home.name}>
-      <nav.Screen name="Home" component={Home} options={{headerShown: false}} />
+    <nav.Navigator initialRouteName={Welcome.name}>
+      <nav.Screen name="Home" component={Welcome} options={{headerShown: false}} />
       <nav.Screen name="Instructions" component={Instructions} />
     </nav.Navigator>
   );
