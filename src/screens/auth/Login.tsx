cc7c9aa7655b370fc181ffefe595e7bf33e42b5c
synchronEnginee <te2.9niku.41cd@gmail.com>
2022-01-29 14:09:28 +0900

    validation add

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,27 +1,71 @@
 import {useUserContext} from 'contexts/UserContext';
+import {useFormik} from 'formik';
 import React, {useCallback} from 'react';
 import {StyleSheet, View} from 'react-native';
-import {Text, Button} from 'react-native-elements';
+import {Button, Input} from 'react-native-elements';
+import * as Yup from 'yup';
 
 export const Login: React.FC = () => {
   const userContext = useUserContext();
 
-  const login = useCallback(() => {
-    // TODO add "catch"
-    userContext.login('user', 'password');
-  }, [userContext]);
+  const login = useCallback(
+    (values: {name: string; password: string}) => {
+      userContext.login(values.name, values.password).then(
+        () => {},
+        () => {},
+      );
+    },
+    [userContext],
+  );
+
+  //formikライブラリはform検証で便利
+  //yupライブラリでバリデーション
+  const formik = useFormik({
+    initialValues: {name: '', password: ''},
+    validationSchema: Yup.object().shape({
+      name: Yup.string().required('名前を入力してください'),
+      password: Yup.string().required('パスワードを入力してください'),
+    }),
+    validateOnChange: false,
+    onSubmit: login,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text h1>ログイン</Text>
-      <Button onPress={login} title="ログイン" />
+    <View style={styles.form}>
+      <Input
+        label="名前"
+        containerStyle={styles.input}
+        autoCapitalize="none"
+        errorMessage={formik.errors.name}
+        onChangeText={formik.handleChange('name')}
+        value={formik.values.name}
+      />
+      <Input
+        label="パスワード"
+        containerStyle={styles.input}
+        errorMessage={formik.errors.password}
+        secureTextEntry
+        onChangeText={formik.handleChange('password')}
+        value={formik.values.password}
+      />
+      <Button
+        disabled={formik.isSubmitting}
+        onPress={() => formik.handleSubmit()}
+        title="ログインする"
+        buttonStyle={styles.button}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  form: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {marginTop: 20, width: '80%'},
+  button: {
+    marginTop: 30,
   },
 });
